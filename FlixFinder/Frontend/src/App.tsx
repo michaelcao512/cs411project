30b0ad2d061322bcdddb8903811259bdeee2d550
graceelias <91794990+graceelias@users.noreply.github.com>
2023-05-04 22:40:13 -0400

    fixed containers and improving routing

--- FlixFinder/Frontend/src/App.tsx
+++ FlixFinder/Frontend/src/App.tsx
@@ -1,19 +1,25 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { myContext } from './Context'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { myContext } from './Context';
 
+import ProtectedRoutes from './ProtectedRoutes';
 import LoginPage from './Containers/LoginPage/LoginPage';
 import ResultsPage from './Containers/ResultsPage/ResultsPage';
 import ErrorPage from './Containers/ErrorPage/ErrorPage';
 
 function App() {
     const userObject = useContext(myContext);
+    var isAuth = true
+    if(userObject == null)
+      isAuth = false
     console.log(userObject);
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage/>}></Route>
-                <Route path="/results" element={<ResultsPage/>}></Route>
+                <Route element={<ProtectedRoutes auth={isAuth}/>}>
+                  <Route path="/results" element={<ResultsPage/>}></Route>
+                </Route>
                 <Route path="/error" element={<ErrorPage/>}></Route>
             </Routes>
         </Router>
---
+++ FlixFinder/Frontend/src/ProtectedRoutes.tsx
@@ -0,0 +1,8 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+const ProtectedRoutes = ({auth}) =>
+{
+    return (auth === true ? <Outlet /> : <Navigate to="/error" replace/>)
+}
+export default ProtectedRoutes;
+
