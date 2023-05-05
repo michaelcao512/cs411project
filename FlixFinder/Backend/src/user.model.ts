3d94be742caf83dcae09e2469e0f18ce28f9e469
Kenneth Thomson <100639963+KennethThomson@users.noreply.github.com>
2023-05-05 17:05:26 -0400

    Update user.model.ts

--- FlixFinder/Backend/src/user.model.ts
+++ FlixFinder/Backend/src/user.model.ts
@@ -7,41 +7,6 @@ const userSchema = new mongoose.Schema({
     },
     username: {
         required: true,
-<<<<<<< HEAD
-        type: String
-=======
-        type: String,
-    },
-    refreshToken: {
-        required: true,
-        type: String,
-    },
-    emotion: {
-        sadness: {
-            type: Number,
-            default: 0
-        },
-        joy: {
-            type: Number,
-            default: 0
-        },
-        fear: {
-            type: Number,
-            default: 0
-        },
-        disgust: {
-            type: Number,
-            default: 0
-        },
-        anger: {
-            type: Number,
-            default: 0
-      }
-    },
-    genres: {
-      type: [Number],
-      default: []
->>>>>>> main
     }
 });
 
